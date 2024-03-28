aa8c16a5208c11932a6bb55faf4c84b5a52b1023
Foysal Hossain <foysalx4@gmail.com>
2024-03-28 22:20:15 +0600

    get-single-item

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ async function run() {
     const db = client.db('cleaning-supplies-l2')
 
     const products = db.collection('products')
-    const flashSale = db.collection('flash-sale')
+    //const flashSale = db.collection('flash-sale')
 
 
     app.post('/products', async (req, res) => {
@@ -39,46 +39,12 @@ async function run() {
         res.send(result)
     })
 
-    app.get('/products/:category', async (req, res) => {
-      const query = {}
-      const result = await products.find(query).toArray()
-      res.send(result)
-
-
-      // const query = req.query.category
-      // const category = query.toLowerCase()
-      // console.log(category);
-
-      // const filterPro = products.filter((p) => p.category.toLowerCase() === category)
-     
-      //   const result = await products.find(filterPro).toArray()
-      //   res.status(200).json({
-      //     success: true,
-      //     message: 'Products retrieved successfully!',
-      //     data: filterPro,
-      //   });
-    })
-
     app.get('/products/:id', async (req, res) => {
         const ids = req.params.id
         const query = {_id: new ObjectId(ids)}
         const result = await products.findOne(query)
         res.send(result)
     })
-
-
-    //flash-sale
-    app.post('/flash-sale', async (req, res) => {
-      const query = req.body
-      const result = await flashSale.insertOne(query)
-      res.send(result)
-  })
-
-    app.get('/flash-sale', async (req, res) => {
-      const query = {}
-      const result = await flashSale.find().toArray()
-      res.send(result)
-  })
 
 
     //Start server
